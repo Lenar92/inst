8b634f91d5c1fec02ee4abbc4687a20b48de8c8c
Student <Student@itpark>
2019-09-28 10:42:47 +0300

    photo&video

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log('worked');
+
+// абстракция - выкидываем все лишнее, что не нужно для решения
+
+const author = {
+    id: 'buzova86',
+    avatarUrl: https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com
+verified: true,
+};
+
+const photo = {
+    id: 'B23d82NooNF'
+    author: author,
+    location: 'Набережная Амура'
+    ImageUrl; 'https://placekitten.com/800/800',
+    videoUrl: null, // специальный маркер, который сообщает что значения нет
+    likes: 159594,
+};
+
+// underfiend и null можно записать куда угодно
+
+//пост с видео
+const video = {
+    id: 'B24F82NooNX'
+    author: author,
+    location: Торгово-развлекательный Центр Острова'
+    imageUrl; 'https://placekitten.com/800/800',
+    videoUrl = 'https://youtube.com/...',
+    likes: 159594,
+};
+
