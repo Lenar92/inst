27e760db7f87f0485dabb441dd5293aa3926b303
Student <Student@itpark>
2019-09-28 10:57:36 +0300

    Переименование переменных

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('worked');
 
 // абстракция - выкидываем все лишнее, что не нужно для решения
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com
 verified: true,
@@ -10,7 +10,7 @@ verified: true,
 
 const photo = {
     id: 'B23d82NooNF'
-    author: author,
+    author: buzova,
     location: 'Набережная Амура'
     ImageUrl; 'https://placekitten.com/800/800',
     videoUrl: null, // специальный маркер, который сообщает что значения нет
@@ -22,7 +22,7 @@ const photo = {
 //пост с видео
 const video = {
     id: 'B24F82NooNX'
-    author: author,
+    author: buzova,
     location: Торгово-развлекательный Центр Острова'
     imageUrl; 'https://placekitten.com/800/800',
     videoUrl = 'https://youtube.com/...',
